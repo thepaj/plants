7047d42a372c8dbc2ff7a43920d41c530d27ebd8
thepaj <pavla.jureckova@gmail.com>
2021-08-30 17:20:33 +0200

    second commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import { Route, Switch } from 'react-router';
+
+import Navigation from './pages/Navigation';
+import About from './pages/About';
+import Home from './pages/Home';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Navigation />
+      <div className='content'>
+        <Switch>
+            <Route exact path="/" component={Home} />
+            <Route path="/about" component={About} />
+            <Route component={Home} />
+        </Switch>
+      </div>
     </div>
   );
 }
---
+++ src/pages/Navigation.js
@@ -0,0 +1,12 @@
+import { Link } from 'react-router-dom';
+
+function Navigation() {
+    return (
+      <div className="navigation">
+        <Link to="/about">About</Link>
+      </div>
+    );
+  }
+  
+  export default Navigation;
+  
